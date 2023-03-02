901160b216335340e786a7f65ceadc8df8d0145a
germanmgs <126623270+germanmgs@users.noreply.github.com>
2023-03-02 09:00:12 -0300

    Update slidebar.js

--- JS/slidebar.js
+++ JS/slidebar.js
@@ -1,4 +1,3 @@
-
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -14,3 +13,22 @@ function openNav() {
     document.getElementById("demo").innerHTML = "elijah.richardson@example.com";
   }
 
+
+let mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
