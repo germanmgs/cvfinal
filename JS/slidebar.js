689d9d9ade161ca60b7948f85b0fb44af07b8792
germanmgs <126623270+germanmgs@users.noreply.github.com>
2023-03-02 11:40:07 -0300

    Update slidebar.js

--- JS/slidebar.js
+++ JS/slidebar.js
@@ -27,17 +27,16 @@ function openNav() {
 
 let mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.addEventListener("load", scrollFunction);
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
 }
 
 function topFunction() {
-  document.body.scrollTop = 0; 
-  document.documentElement.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
 }
